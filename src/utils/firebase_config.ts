fca3159dddc897bafe11cd1846e6559b555c2070
akirillof <alexkirillof@yandex.ru>
2023-12-10 21:48:38 +0300

    10_12

---
+++ src/utils/firebase_config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCqxTtWhUD2AlA2I7o7j0Yc3a9HNOm9MBw',
+  authDomain: 'mycostsgb.firebaseapp.com',
+  databaseURL: 'https://mycostsgb-default-rtdb.firebaseio.com',
+  projectId: 'mycostsgb',
+  storageBucket: 'mycostsgb.appspot.com',
+  messagingSenderId: '190430056384',
+  appId: '1:190430056384:web:49d4dbe03cdf2ec5574225',
+  measurementId: 'G-R1DXE582H9',
+}
